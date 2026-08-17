76c493eba1eb006cfb7d7fd4bbdfa8cac4812f1f
RyanNZZ2205 <ryanng2205@gmail.com>
2026-08-17 11:37:09 +0800

    Add welcome and farewell message

--- src/main/java/ErmActually.java
+++ src/main/java/ErmActually.java
@@ -1,8 +1,24 @@
+/**
+ * Starts ErmActually, which greets the user and then exits.
+ */
 public class ErmActually {
     public static void main(String[] args) {
-        String banner = " +----------------+\n"
+        String banner = "+----------------+\n"
                 + "|  Erm Actually  |\n"
-                + "+----------------+\n";
+                + "+----------------+";
+
+        String line = "____________________________________________________________";
+
+        String welcome = "Greetings! I'm Erm Actually.\n"
+                + "How can I actually help you today?";
+
+        String farewell = "Farewell! Hope you stop by again!";
+
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println(welcome);
+        System.out.println(line);
+        System.out.println(farewell);
+        System.out.println(line);
     }
 }
