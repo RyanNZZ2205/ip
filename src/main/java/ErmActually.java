83c69a09d45043591cb76362ce4a46816f55919b
RyanNZZ2205 <ryanng2205@gmail.com>
2026-08-17 11:19:14 +0800

    Rename to Erm Actually
    Changed Duke.java to ErmActually.java

---
+++ src/main/java/ErmActually.java
@@ -0,0 +1,8 @@
+public class ErmActually {
+    public static void main(String[] args) {
+        String banner = " +----------------+\n"
+                + "|  Erm Actually  |\n"
+                + "+----------------+\n";
+        System.out.println(banner);
+    }
+}
